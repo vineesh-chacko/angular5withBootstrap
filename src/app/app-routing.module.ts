09291de470082967bb45b0e8975f3ced7ef5313a
Vineesh Chacko <vineesh.chacko@gmail.com>
2018-11-26 22:33:24 +0000

    First_commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { UserComponent } from "./user/user.component";
+
+const routes: Routes = [{ path: "home", component: UserComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
